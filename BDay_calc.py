c57f55a8ac40023a4f8a71ddf2142653b60fea59
Soli-12 <solijooni12@gmail.com>
2024-07-19 14:28:56 +0200

    Update BDay_calc.py
    Year is added as the prompt

--- BDay_calc.py
+++ BDay_calc.py
@@ -1,4 +1,5 @@
 # first python file calculate the birth year
+Year = input('Which year is it:')
 birth_year = input('Birth year:')
-age = 2024 - int(birth_year)
+age = int(Year)- int(birth_year)
 print ("your age is:",age)
